7ca71aae5151f0aabbff04da5356546cfaceb900
Justin-12138 <2094623381@qq.com>
2023-08-08 20:24:39 +0800

    3maincor_euclidean

---
+++ filter/correlation_coefficient.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from scipy.stats import pearsonr, spearmanr, kendalltau
+from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
+pea = []
+spea = []
+kendal = []
+euclid = []
+data = pd.read_csv('../data/iris.csv')
+data['class'], _ = pd.factorize(data['class'])
+print(list(data.columns))
+
+fea = ['f1', 'f2', 'f3', 'f4']
+# features = ['sepallength', 'sepalwidth', 'petallength', 'petalwidth']
+
+for feature in list(data.columns)[0:-1]:
+    # 计算皮尔逊相关系数
+    pearson_corr, _ = pearsonr(data[feature], data['class'].astype(float))
+    # 计算斯皮尔曼相关系数
+    spearman_corr, _ = spearmanr(data[feature], data['class'].astype(float))
+    # 计算肯德尔秩相关系数
+    kendall_corr, _ = kendalltau(data[feature], data['class'].astype(float))
+    # 计算欧几里德距离
+    euclidean_dist = euclidean(data[feature], data['class'].astype(float))
+    print(
+        f'({feature}, class): ({abs(pearson_corr):.4f}, {abs(spearman_corr):.4f}, {abs(euclidean_dist):.4f}, {abs(kendall_corr):.4f}')
+    pea.append(pearson_corr)
+    spea.append(spearman_corr)
+    kendal.append(kendall_corr)
+    euclid.append(euclidean_dist)
+
+plt.subplot(2, 2, 1)
+plt.plot(fea, pea, linestyle='--', color='blue')
+plt.title("pea")
+
+plt.subplot(2, 2, 2)
+plt.plot(fea, spea, linestyle='-.', color='red')
+plt.title("spea")
+
+plt.subplot(2, 2, 3)
+plt.plot(fea, kendal, linestyle='dashed', color='green')
+plt.title("kendal")
+
+plt.subplot(2, 2, 4)
+plt.plot(fea, euclid, linestyle=':', color='black')
+plt.title("euclid")
+plt.tight_layout()  # adjust the pic
+plt.show()
